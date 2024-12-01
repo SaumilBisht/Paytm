cae0417996f1ee39572cdfb29bf08ad845fd9a10
SaumilBisht <bishtsaumil@gmail.com>
2024-12-01 10:35:05 +0530

    Account collection in db.js

--- backend/root/db.js
+++ backend/root/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 
 mongoose.connect("mongodb+srv://bishtsaumil:1gupbDqMS8dIYz63@cluster0.9p1qw.mongodb.net/paytm")
 
-const userSchema=mongoose.Schema({
+const userSchema=new mongoose.Schema({
   username:String,
   password:String,
   firstName:String,
@@ -11,4 +11,21 @@ const userSchema=mongoose.Schema({
 
 const User=mongoose.model("User",userSchema);
 
-module.exports={User}
+const accountSchema=new mongoose.Schema({
+  userId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User',
+    required:true
+  },
+  balance:{
+    type:Number,
+    required:true
+  }
+})
+
+const Account=mongoose.model('Account',accountSchema);
+
+module.exports={
+  User,
+  Account
+}
